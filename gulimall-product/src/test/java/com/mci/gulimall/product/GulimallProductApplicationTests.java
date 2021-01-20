cd09bb11dbbe854a189fbcba780b5cf956666abc
gzmarkchoi <gzmarkchoi@gmail.com>
2021-01-20 11:23:19 +0100

    - back to JUnit 5 - "Admin" Vue project

--- gulimall-product/src/test/java/com/mci/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/mci/gulimall/product/GulimallProductApplicationTests.java
@@ -4,14 +4,11 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.mci.gulimall.product.entity.BrandEntity;
 import com.mci.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
 

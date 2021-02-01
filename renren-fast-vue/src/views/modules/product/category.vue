27ea021491201c2a8a70d97625d77bf6608256e1
gzmarkchoi <gzmarkchoi@gmail.com>
2021-02-01 23:00:17 +0100

    - delete "Category" - modify "Category"

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,33 +1,64 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox
-    node-key="catId"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button type="text" size="mini" @click="edit(data)">
+            Edit
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span></el-tree
+    >
+    <el-dialog
+      :title="dialogTitle"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :close-on-click-modal="false"
+    >
+      <el-form :model="category">
+        <el-form-item label="Category Name">
+          <el-input v-model="category.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Category icon">
+          <el-input v-model="category.icon" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Category product unit">
+          <el-input
+            v-model="category.productUnit"
+            auto-complete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
       <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          Append
-        </el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="addCategory">OK</el-button>
       </span>
-    </span></el-tree
-  >
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -40,7 +71,21 @@ export default {
   props: {},
   data() {
     return {
+      dialogTitle: "",
+      dialogType: "", // edit or add
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        productUnit: "",
+        icon: "",
+        catId: null,
+      },
+      dialogVisible: false,
       menus: [],
+      expandedKey: [],
       defaultProps: {
         children: "children",
         label: "name",
@@ -55,15 +100,118 @@ export default {
         url: this.$http.adornUrl("/product/category/list/tree"),
         method: "get",
       }).then(({ data }) => {
-        console.log("成功获取到菜单数据...", data.data);
+        console.log("get menus data successfully...", data.data);
         this.menus = data.data;
+      });
+    },
+    edit(data) {
+      console.log("edit menu item", data);
+      this.dialogType = "edit";
+      this.dialogVisible = true;
+      this.dialogTitle = "Edit category";
+
+      // Refresh category data from DB
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
       });
     },
     append(data) {
       console.log("append", data);
+      this.dialogType = "add";
+      this.dialogVisible = true;
+      this.dialogTitle = "Add category";
+      this.category.parentCid = data.catId;
+      this.category.catId = data.catLevel * 1 + 1;
     },
-
+    submitData() {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      }
+      if (this.dialogType == "edit") {
+        this.editCategory();
+      }
+    },
+    editCategory() {
+      var { catId, name, icon, productUnit } = this.category;
+      // key is the same as Java Bean
+      /*
+      var data = {
+        catId: catId,
+        name: name,
+        icon: icon,
+        productUnit: productUnit,
+      }; */
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "category modified",
+          type: "success",
+        });
+        // Close dialog box
+        this.dialogVisible = false;
+        // Refresh menus
+        this.getMenus();
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+    // Add level 3 category menu
+    addCategory() {
+      console.log("Add level 3 category", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "category saved",
+          type: "success",
+        });
+        // Close dialog box
+        this.dialogVisible = false;
+        // Refresh menus
+        this.getMenus();
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
     remove(node, data) {
+      var ids = [data.catId];
+      // confirm box
+      this.$confirm(
+        `It would delete ${data.name} menu, are you sure?`,
+        "Warning",
+        {
+          confirmButtonText: "Yes",
+          cancelButtonText: "No",
+          type: "warning",
+        }
+      )
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
+            this.$message({
+              message: "menu deleteded",
+              type: "success",
+            });
+            this.getMenus();
+            // set the default expanded menu
+            this.expandedKey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {});
+
       console.log("remove", node, data);
     },
   },
